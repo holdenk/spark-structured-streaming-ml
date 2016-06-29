ff3a92c779e11919dea01966ac31ccb8002aecc2
Holden Karau <holden@us.ibm.com>
2016-06-28 17:40:03 -0700

    Start adding a sample custom sink

---
+++ src/main/scala/com/high-performance-spark-examples/structuredstreaming/CustomSink.scala
@@ -0,0 +1,32 @@
+/**
+ * A simple custom sink to allow us to train our models on micro-batches of data.
+ */
+package com.highperformancespark.examples.structuredstreaming
+
+import org.apache.spark._
+import org.apache.spark.sql.{Dataset, DataFrame, Encoder, SQLContext}
+import org.apache.spark.sql.sources.StreamSinkProvider
+import org.apache.spark.sql.execution.streaming.Sink
+
+/**
+ * Creates a custom sink similar to the old foreachRDD. Provided function is called for each
+ * time slice with the dataset representing the time slice.
+ * Provided func must consume the dataset (e.g. call `foreach` or `collect`).
+ */
+case class ForeachDatasetSinkProvider[T: Encoder](func: Dataset[T] => Unit)
+    extends StreamSinkProvider {
+  def createSink(sqlContext: SQLContext, params: Map[String, String], cols: Seq[String]) = {
+    new ForeachDatasetSink(func)
+  }
+}
+
+/**
+ * Custom sink similar to the old foreachRDD. Do not construct directly, instead provide
+ * [[ForeachDatasetSinkProvider]] to Spark's DataStreamWriter format.
+ */
+case class ForeachDatasetSink[T: Encoder](func: Dataset[T] => Unit)
+    extends Sink {
+  def addBatch(batchId: Long, data: DataFrame): Unit = {
+    func(data.as[T])
+  }
+}
