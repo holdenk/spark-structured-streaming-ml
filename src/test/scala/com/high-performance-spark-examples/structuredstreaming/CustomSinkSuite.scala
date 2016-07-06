0a010601e28c1f87e2d45d02fe2eb574bc788ac8
Holden Karau <holden@us.ibm.com>
2016-07-06 12:44:54 -0700

    fuck 2.0.0-preview is missing write stream...

--- src/test/scala/com/high-performance-spark-examples/structuredstreaming/CustomSinkSuite.scala
+++ src/test/scala/com/high-performance-spark-examples/structuredstreaming/CustomSinkSuite.scala
@@ -3,12 +3,30 @@
  */
 package com.highperformancespark.examples.structuredstreaming
 
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+
 import scala.collection.mutable.ListBuffer
+
+import org.scalatest.FunSuite
 
 import org.apache.spark._
 import org.apache.spark.sql.{Dataset, DataFrame, Encoder, SQLContext}
+import org.apache.spark.sql.execution.streaming.MemoryStream
 
-class CustomSinkSuite {
+class CustomSinkSuite extends FunSuite with DataFrameSuiteBase {
+
+  protected implicit def impSqlContext: SQLContext = sqlContext
+
+  test("really simple test of the custom sink") {
+    import spark.implicits._
+    val input = MemoryStream[String]
+    val doubled = input.toDS().map(x => x + " " + x)
+    doubled.writeStream
+      .queryName("testCustomSinkBasic")
+      .format("com.highperformancespark.examples.structuredstreaming")
+      .start()
+    input.addData(List("hi", "holden", "bye", "pandas"))
+  }
 }
 
 /**
